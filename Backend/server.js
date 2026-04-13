b59c772a5781789804072028d86a3dd313b2046c
Tharun <tharun.wasantha@gmail.com>
2026-04-13 20:09:29 +0530

    port change frontend and backend

--- Backend/server.js
+++ Backend/server.js
@@ -60,7 +60,8 @@ app.use((req, res, next) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
