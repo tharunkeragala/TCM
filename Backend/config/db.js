52ff63eca7500e12b062ee49ccc5f283d545b9f7
Tharun <tharun.wasantha@gmail.com>
2026-05-01 09:44:02 +0530

    db file changes

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,14 +1,27 @@
 const sql = require("mssql");
 
+// const config = {
+//   user: "app_user",
+//   password: "password123",
+//   server: "localhost",
+//   port: 1433,
+//   database: "test_case_manager",
+//   options: {
+//     trustServerCertificate: true,
+//     encrypt: false
+//   }
+// };
+
+
 const config = {
   user: "app_user",
   password: "password123",
-  server: "localhost",
-  port: 1433,
+  server: "DESKTOP-NF8MTUG",
   database: "test_case_manager",
   options: {
-    trustServerCertificate: true,
-    encrypt: false
+    instanceName: "SQLEXPRESS02",
+    encrypt: true,
+    trustServerCertificate: true
   }
 };
 
